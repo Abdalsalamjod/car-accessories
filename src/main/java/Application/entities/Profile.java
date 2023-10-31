76d3b079570f0f28c13ed340e785b92120aad1b7
Abd-Alsalam-Jodalah <129756251+Abd-Alsalam-Jodalah@users.noreply.github.com>
2023-10-31 02:14:37 +0200

    creat profile feature

--- src/main/java/Application/entities/Profile.java
+++ src/main/java/Application/entities/Profile.java
@@ -1,5 +1,43 @@
 package Application.entities;
 
-public class Profile {
+import Application.InstallationRequest;
+import Application.Order;
 
+import java.util.List;
+import java.util.ArrayList;
+
+public class Profile {
+    private User user;
+    private List<Order> orderHistory;
+    private List<InstallationRequest> installationRequests;
+
+    public Profile(User user) {
+        this.user = user;
+        this.orderHistory = new ArrayList<>();
+        this.installationRequests = new ArrayList<>();
+    }
+
+    // Methods to manage user profile
+    public void updateUserProfile(String newEmail, String newPassword) {
+        user.setEmail(newEmail);
+        user.setPassword(newPassword);
+    }
+
+    // Methods to manage order history
+    public List<Order> getOrderHistory() {
+        return orderHistory;
+    }
+
+    public void addOrderToHistory(Order order) {
+        orderHistory.add(order);
+    }
+
+    // Methods to manage installation requests
+    public List<InstallationRequest> getInstallationRequests() {
+        return installationRequests;
+    }
+
+    public void addInstallationRequest(InstallationRequest request) {
+        installationRequests.add(request);
+    }
 }

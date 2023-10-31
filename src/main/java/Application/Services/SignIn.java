0107fe67a30df176e5596f38a7ab6e1727109b77
Abd-Alsalam-Jodalah <129756251+Abd-Alsalam-Jodalah@users.noreply.github.com>
2023-10-31 02:13:51 +0200

    fix SignIn feature

--- src/main/java/Application/Services/SignIn.java
+++ src/main/java/Application/Services/SignIn.java
@@ -1,23 +1,40 @@
 package Application.Services;
 
+import Application.entities.User;
+
 public class SignIn {
-    public boolean signedIn;
-    public boolean valid;
     public String email;
     public String password;
-
-
-
+    public String role;
+    public boolean signedIn;
+    public int validationStatus;
     public SignIn() {
-        signedIn=false;
-        valid=false;
         email=null;
         password=null;
+        role=null;
+        signedIn=false;
+        validationStatus=-1;
     }
 
-    public String getMessage() {
-        if (valid )
-            return "successful sign in\n";
-        return "xxxxx";
+    public SignIn(String email, String password, String role, boolean signedIn, int validationStatus) {
+        this.email=email;
+        this.password=password;
+        this.role=role;
+        this.signedIn=signedIn;
+        this.validationStatus=validationStatus;
     }
+
+    public User performLogIn() {
+        if (validationStatus==0){
+            this.signedIn=true;
+            // connect with DB
+            return new User(email, password, role, signedIn);
+        }
+        else {
+            this.signedIn=false;
+            return null;
+        }
+
+    }
+
 }
--- src/test/resources/features/Services/SignIn.feature
+++ src/test/resources/features/Services/SignIn.feature
@@ -2,17 +2,14 @@ Feature: User Login
 
   Scenario: User logs in successfully with valid credentials
     Given the user accesses the sign-in command
-    When the user enters "valid@example.com" , "validpass"
-#    And confirms the login
+    When the user provides valid information
     Then the user should be successfully logged in
-    And should be redirected to the user dashboard
+#    And should be redirected to the user dashboard
 
-Feature: User Login
 
   Scenario Outline: User login with invalid credentials
     Given the user accesses the sign-in command
-    When the user enters "<username>" , "<password>"
-    #    And confirms the login
+    When the user provides information '<username>' , '<password>'
     Then the system should display an error message "<error_message>"
     And the user should not be logged in
 

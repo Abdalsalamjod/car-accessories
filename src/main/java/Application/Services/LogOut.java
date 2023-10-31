c30cebb54a1e44f7514d4b7638305cf68b22aa6a
Abd-Alsalam-Jodalah <129756251+Abd-Alsalam-Jodalah@users.noreply.github.com>
2023-10-31 02:12:46 +0200

    creat logout feature

--- src/main/java/Application/Services/LogOut.java
+++ src/main/java/Application/Services/LogOut.java
@@ -1,2 +1,10 @@
-package Application.Services;public class LogOut {
+package Application.Services;
+
+import Application.entities.User;
+
+public class LogOut {
+        public User performLogout(User user) {
+           user =null;
+           return user;
+        }
 }

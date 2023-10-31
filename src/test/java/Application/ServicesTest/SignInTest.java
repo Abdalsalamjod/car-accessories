d59ee0aa6755c6bd3ea704f36d9ec1cd070c48d6
Abd-Alsalam-Jodalah <129756251+Abd-Alsalam-Jodalah@users.noreply.github.com>
2023-10-31 02:13:58 +0200

    fix SignIn feature

--- src/test/java/Application/ServicesTest/SignInTest.java
+++ src/test/java/Application/ServicesTest/SignInTest.java
@@ -1,6 +1,7 @@
 package Application.ServicesTest;
 
 import Application.LoggerUtility;
+import Application.Services.MessagesGenerator;
 import Application.Services.SignIn;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -8,61 +9,50 @@ import io.cucumber.java.en.When;
 
 import java.util.logging.Logger;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static Application.Services.ValidationUser.validation;
+import static org.junit.Assert.*;
 
 public class SignInTest {
     private static Logger logger = LoggerUtility.getLogger();
     SignIn signIn ;
     public SignInTest() {
         signIn=new SignIn();
+        signIn.email="valid@example.com";
+        signIn.password="validpass";
     }
-
-
-
     @Given("the user accesses the sign-in command")
     public void theUserAccessesTheSignInCommand() {
         assertFalse(signIn.signedIn);
         logger.info("Please Enter your Email and password\n");
     }
-
-    @When("the user enters {string} , {string}")
-    public void theUserEnters(String string, String string2) {
-        signIn.email=string;
-        signIn.password=string2;
-
-        logger.info( "Email: "+signIn.email+ "\nPassword: "+signIn.password+"\n");
-        signIn.valid= signIn.email.equals("valid@example.com") && signIn.password.equals("validpass");
+    @When("the user provides valid information")
+    public void theUserProvidesValidInformation() {
+      signIn.validationStatus=  validation(signIn.email,signIn.password);
+       assertEquals(signIn.validationStatus,0);
     }
-//    @When("confirms the login")
-//    public void confirmsTheLogin() {
-////        assertTrue(signIn.valid);
-//        logger.info(signIn.getMessage());
-//    }
-
     @Then("the user should be successfully logged in")
     public void theUserShouldBeSuccessfullyLoggedIn() {
-        logger.info(signIn.getMessage());
-        signIn.signedIn  =true;
+
+        signIn.performLogIn();
         assertTrue(signIn.signedIn);
-
+        logger.info(MessagesGenerator.SigningMessages(signIn.validationStatus));
     }
-//    @Then("should be redirected to the user dashboard")
-//    public void shouldBeRedirectedToTheUserDashboard() {
-//
-//        logger.info("you are in user dashboard "+"\n");
-//    }
 
 
 
+    @When("the user provides information {string} , {string}")
+    public void theUserProvidesInformation(String Email, String Password) {
+        signIn.validationStatus=validation(Email,Password);
+        assertNotEquals(signIn.validationStatus,0);
+    }
     @Then("the system should display an error message {string}")
     public void theSystemShouldDisplayAnErrorMessage(String string) {
-        logger.info( string+"\n");
+        logger.info(MessagesGenerator.SigningMessages(signIn.validationStatus));
     }
 
     @Then("the user should not be logged in")
     public void theUserShouldNotBeLoggedIn() {
-        signIn.signedIn=false;
+        signIn.performLogIn();
         assertFalse( signIn.signedIn);
     }
 

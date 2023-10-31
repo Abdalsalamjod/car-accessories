75e42770d4c76c1caefcf4f6e91657653999267b
Abd-Alsalam-Jodalah <129756251+Abd-Alsalam-Jodalah@users.noreply.github.com>
2023-10-31 02:14:11 +0200

    fix SignUp feature

--- src/main/java/Application/Services/SignUp.java
+++ src/main/java/Application/Services/SignUp.java
@@ -1,60 +1,37 @@
 package Application.Services;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import Application.entities.User;
+import javax.swing.JOptionPane;
 
 public class SignUp {
 
-    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@(.+)$";
-    public  boolean valid;
-    public boolean hasAccount;
     public String email;
     public String password;
-    public String message;
+    public boolean hasAccount;
+    public int validationStatus;
 
-
-
+    SignIn signIn;
     public SignUp() {
-        message="empty String, put if you see this messge in any case -> there an error in signup";
+        email=null;
+        password=null;
         hasAccount=false;
-        valid=false;
-    }
-    public String generateMessage() {
-        //maybe this code have a problem , becos i nedd database
-        if (!isValidEmail(this.email)) {
-            message = "Invalid email address\n";
-        } else if (!isExistEmail(this.email)) {
-            message = "Email address is already in use\n";
-        } else if(email==null || email.equals(" ")){
-            message ="Empty, not valid empty Email";
-        }
-        else {
-            message = "Successful Registration, you are welcome\n";
-        }
-        return message;
+        validationStatus =-1;
+
     }
 
+    public void creatAccount() {
+        if (validationStatus == 0) {
 
-    public void validation(String email, String password) {
-        valid= isValidEmail(email) && isExistEmail(email) && ( email != null) && (!email.equals(" ") );
-    }
-
-    public static boolean isValidEmail(String email) {
-        Pattern pattern = Pattern.compile(EMAIL_REGEX);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
-    }
-    private boolean isExistEmail(String email) {
-         // check data in DB
-        return false;
-    }
-
-    public void creatAccount(String email, String password) {
-        if (valid){
             // this function should put data in DB
-            this.hasAccount=true;
+            this.hasAccount = true;
+        }
+        else
+        {
+            this.hasAccount = false;
         }
     }
-
+    public User performLogIn() {
+      signIn=new SignIn(email, password, "user", false, validationStatus);
+      return signIn.performLogIn();
+    }
 
 }

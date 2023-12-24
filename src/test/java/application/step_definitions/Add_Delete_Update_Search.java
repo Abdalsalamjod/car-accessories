d8036139ffed824b45590c91b078386310810c58
shehab-kharaz <shehababbaskharaz.222@hotmail.com>
2023-12-24 20:06:59 +0200

    delete product test cases to test Sonar

--- src/test/java/application/step_definitions/Add_Delete_Update_Search.java
+++ src/test/java/application/step_definitions/Add_Delete_Update_Search.java
@@ -14,41 +14,34 @@ import static org.junit.Assert.*;
 public class Add_Delete_Update_Search {
 
   private static final DatabaseService dbs = new DatabaseService();
-  private static boolean flagBetweenSteps;
-  private static String messageBetweenSteps;
-  private final Admin newAdmin = new Admin("s12027747@stu.najah.edu", "12345", 'a', true, null);
-  private final User newUser = new User("s12027747@stu.najah.edu", "12345", 'u', true, null);
+
 
 
   //**********************FOR ADD*********************************
   @When("I add a new valid product")
   public void i_add_a_new_valid_product() {
-    flagBetweenSteps = false;
-    newAdmin.manageProducts(2, dbs);
-    flagBetweenSteps = true;
+
   }
   @Then("the product should be added to the database")
   public void the_product_should_be_added_to_the_database() {
-    assertTrue(flagBetweenSteps);
   }
 
   @When("I add a duplicated-Id product")
   public void i_add_a_duplicated_id_product() {
-    i_add_a_new_valid_product();
+
   }
   @Then("the product should not be added to the database")
   public void the_product_should_not_be_added_to_the_database() {
-    assertFalse(flagBetweenSteps);
+
   }
 
   @When("I add a new product with invalid  ID {int}, name {string}, category {string}, price {double}, quantity {int}")
   public void i_add_a_new_product_with_invalid_id_name_category_price_quantity(Integer id, String name, String category, Double price, Integer quantity) {
-    Product newProduct = new Product(id, name, category, price, quantity);
-    messageBetweenSteps = newProduct.validInformation();
+
   }
   @Then("the product should not be added to the database and the error message should be equals to {string}")
   public void the_product_should_not_be_added_to_the_database_and_the_error_message_should_be_equals_to(String errorMessage) {
-    assertEquals(messageBetweenSteps, errorMessage);
+
   }
   //*************************************************************
 
@@ -58,22 +51,20 @@ public class Add_Delete_Update_Search {
   //*************************FOR DELETE**************************
   @When("the admin deletes an existing product")
   public void the_admin_deletes_an_existing_product() {
-    flagBetweenSteps = false;
-    newAdmin.manageProducts(3, dbs);
-    flagBetweenSteps = true;
+
   }
   @Then("the product should be removed from the database")
   public void the_product_should_be_removed_from_the_database() {
-    assertTrue(flagBetweenSteps);
+
   }
 
   @When("the admin deletes a non-existing product")
   public void the_admin_deletes_a_non_existing_product() {
-    the_admin_deletes_an_existing_product();
+
   }
   @Then("nothing will happen")
   public void nothing_will_happen() {
-    assertTrue(true);
+
   }
   //*************************************************************
 
@@ -84,29 +75,19 @@ public class Add_Delete_Update_Search {
   @When("admin updates the product with ID {int} to have a new price {double}, and quantity {int}")
   public void admin_updates_the_product_with_id_to_have_a_new_price_and_quantity(Integer id, Double price, Integer quantity) {
 
-    try{
-        if(price > 0 && quantity > 0) {
-          Product oldProduct = dbs.executeQuery("SELECT * FROM Product WHERE id=" + id, new ProductResultHandler());
-          Product newProduct = new Product(oldProduct.getId(), oldProduct.getName(), oldProduct.getCategory(), price, quantity);
-          dbs.updateObject(newProduct, "Product", "id");
-        }
-        flagBetweenSteps = true;
-      }catch ( Exception e ){
-        flagBetweenSteps = false;
-      }
+
   }
   @Then("the product information should be updated in the database")
   public void the_product_information_should_be_updated_in_the_database() {
-    assertTrue(flagBetweenSteps);
+
   }
   @When("admin updates the product with ID {int} to have an invalid {double} andOR invalid {int}")
   public void admin_updates_the_product_with_id_to_have_an_invalid_and_or_invalid(Integer id, Double price, Integer quantity) {
-    Product testProduct = new Product(id, "testProduct", "exterior", price, quantity);
-    messageBetweenSteps = testProduct.validInformation();
+
   }
   @Then("the error message should be equals to {string}")
   public void the_error_message_should_be_equals_to(String errorMessage) {
-    assertEquals(messageBetweenSteps, errorMessage);
+
   }
 
   //**************************************************************
